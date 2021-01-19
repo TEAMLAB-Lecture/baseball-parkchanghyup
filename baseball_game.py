b81228a4eeb6e47e79dc22ff27777626fc1ea810
hyup <ariz1623@g.kmou.ac.kr>
2021-01-19 18:52:59 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -18,8 +18,8 @@ def is_digit(user_input_number):
 
 
 def is_between_100_and_999(user_input_number):
-
-    if int(user_input_number) >= 100 and int(user_input_number) <= 999:
+    user_input_number = int(user_input_number)
+    if user_input_number >= 100 and user_input_number <= 999:
         return True
     else:
         return False
@@ -27,10 +27,8 @@ def is_between_100_and_999(user_input_number):
 
 def is_duplicated_number(three_digit):
 
-    check = set()
+    check = set(three_digit)
 
-    for i in three_digit:
-        check.add(i)
 
     if len(check) == 3:
         return False
@@ -95,36 +93,44 @@ def is_no(one_more_input):
 
 def main():
     print("Play Baseball")
-
+    random_number = str(get_not_duplicated_three_digit_number())
+    print("Random Number is : ", random_number)
     
     
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
     check =True
     while check:
-        random_number = str(get_not_duplicated_three_digit_number())
-        print("Random Number is : ", random_number)
-        while True:
-            user_input = input('Input guess number : ')
-            if user_input =='0' :
-                check = False
-                break
-                
-            if is_validated_number(user_input):
-                strikes, balls = get_strikes_or_ball(user_input, random_number)
-                print('strikes : ', strikes, 'balls : ', balls)
-                if strikes == 3:
-                    print('You win, one more(Y/N)?')
-                    if is_yes(input()):
+
+        user_input = input('Input guess number : ')
+        if user_input =='0' :
+            check = False
+            break
+
+        if is_validated_number(user_input):
+            strikes, balls = get_strikes_or_ball(user_input, random_number)
+            print('strikes : ', strikes, 'balls : ', balls)
+            if strikes == 3:
+                while True:
+                    answer = input('You win, one more(Y/N)?')
+
+                    if is_yes(answer):
                         check =True
+                        random_number = str(get_not_duplicated_three_digit_number())
+                        print("Random Number is : ", random_number)
                         break
 
-                    else:
-                        check=False
+                    elif is_no(answer):
+                        check = False
                         break
 
-            else:
-                print('Wrong Input, Input again')
+                    else :
+                        print('Wrong Input, Input again')
+
+
+
+        else:
+            print('Wrong Input, Input again')
 
     # ==================================
     print("Thank you for using this program")
